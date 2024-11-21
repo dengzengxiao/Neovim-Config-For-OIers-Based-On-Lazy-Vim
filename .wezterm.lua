36c81683b3c5d237f118e35c5824feba78047c73
dengzengxiao <dengzengxiao___@outlook.com>
2024-11-21 17:36:47 +0800

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,8 +3,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Mocha"
-config.default_cwd = "D:\\dengzengxiao_projects\\cpp"
-config.font = wezterm.font("Fira Code")
+config.font = wezterm.font("JetBrainsMono NF")
 config.font_size = 16
 config.default_cursor_style = "SteadyBar"
 
