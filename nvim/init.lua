93a677f00df165a680db42cc394b1ca4bd463352
SuperKevinZhou <zzh__2020@outlook.com>
2025-01-16 12:59:29 +0800

    Update init.lua for Neovide config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,18 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 
 if vim.g.neovide then
-    vim.o.guifont = "CommitMono Nerd Font:h14"
+    vim.o.guifont = "JetBrainsMono Nerd Font:h12"
     vim.g.neovide_refresh_rate = 165
+    vim.g.neovide_transparency = 0.8
+    vim.g.neovide_normal_opacity = 0.8
+    vim.g.neovide_theme = 'dark'
+    vim.g.neovide_cursor_vfx_mode = "railgun"
+    vim.g.neovide_cursor_vfx_particle_density = 20.0
+    vim.g.neovide_title_background_color = string.format(
+        "%x",
+        vim.api.nvim_get_hl(0, {id=vim.api.nvim_get_hl_id_by_name("Normal")}).bg
+    )
+    vim.g.neovide_title_text_color = "pink"
 end
 
 require("config.lazy")
