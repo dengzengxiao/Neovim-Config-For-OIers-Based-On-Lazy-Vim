9123e25cab701e3af3df3ea22fbc072584463397
SuperKevinZhou <zzh__2020@outlook.com>
2025-01-16 16:21:53 +0800

    Update keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,6 +6,8 @@ local opts = {
     silent = true,
 }
 
+vim.keymap.set("n", "q<esc>", "<Cmd>q<CR>", opts)
+vim.keymap.set("n", "<C-a>", "ggVGy", opts)
 vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
 vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
 vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
@@ -15,4 +17,6 @@ vim.keymap.set("i", "<F11>", "<Cmd>CompetiTest run<CR>", opts)
 vim.keymap.set("n", "<C-S-x>", "<Cmd>CompetiTest receive testcases<CR>", opts)
 vim.keymap.set("i", "<C-S-x>", "<Cmd>CompetiTest receive testcases<CR>", opts)
 vim.keymap.set("i", "<kOrigin>", "<esc>", opts)
-vim.keymap.set("i", "jj", "<esc>", opts)
+vim.keymap.set("n", "jj", "<Cmd>lua vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen<CR>", opts)
+vim.keymap.set("n", "opa", "<Cmd>lua vim.g.neovide_normal_opacity = vim.g.neovide_normal_opacity + 0.1; vim.g.neovide_transparency = vim.g.neovide_transparency + 0.1<CR>", opts)
+vim.keymap.set("n", "opm", "<Cmd>lua vim.g.neovide_normal_opacity = vim.g.neovide_normal_opacity - 0.1; vim.g.neovide_transparency = vim.g.neovide_transparency - 0.1<CR>", opts)
